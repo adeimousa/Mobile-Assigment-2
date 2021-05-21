c1a4e51ba1a1562bbc65ee65fef0f77bd2a374f8
Adei <adeihim99@gmail.com>
2021-05-21 21:07:24 +0300

    add connection to student

--- app/src/main/java/edu/birzeit/mobileassigment2/activities/MainActivity.java
+++ app/src/main/java/edu/birzeit/mobileassigment2/activities/MainActivity.java
@@ -2,9 +2,11 @@ package edu.birzeit.mobileassigment2.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import edu.birzeit.mobileassigment2.R;
+import edu.birzeit.mobileassigment2.activities.student.StudentActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, StudentActivity.class);
+        startActivity(intent);
     }
 }
