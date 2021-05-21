56dbecc49cc5e5b5c9b39fa9b38ab025273d2078
Ammar-Alhazinah <62383932+Ammar-Alhazinah@users.noreply.github.com>
2021-05-21 21:30:12 +0300

    Revert "add connection to student"

--- app/src/main/java/edu/birzeit/mobileassigment2/activities/StudentActivity.java
+++ app/src/main/java/edu/birzeit/mobileassigment2/activities/StudentActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import edu.birzeit.mobileassigment2.R;
 
-public class MainActivity extends AppCompatActivity {
+public class StudentActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_student);
     }
 }
